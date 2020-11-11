e9decc9154f227e21fb9a2c680ace3b28cc4a21a
Dylan Yung <YungDexiong@hotmail.com>
2020-11-11 03:10:14 -0800

    samples probabilistically from name dist

--- train.py
+++ train.py
@@ -10,54 +10,56 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--name', help='Session name', type=str, default='first')
 parser.add_argument('--batch_size', help='batch_size', type=int, default=128)
 parser.add_argument('--latent_size', help='latent_size', type=int, default=200)
-parser.add_argument(
-    '--unit_size', help='unit_size of rnn cell', type=int, default=512)
-parser.add_argument(
-    '--n_rnn_layer', help='number of rnn layer', type=int, default=3)
+parser.add_argument('--unit_size',
+                    help='unit_size of rnn cell',
+                    type=int,
+                    default=512)
+parser.add_argument('--n_rnn_layer',
+                    help='number of rnn layer',
+                    type=int,
+                    default=3)
 parser.add_argument('--prop_file', help='name of property file', type=str)
 parser.add_argument('--mean', help='mean of VAE', type=float, default=0.0)
 parser.add_argument('--stddev', help='stddev of VAE', type=float, default=1.0)
 parser.add_argument('--num_epochs', help='epochs', type=int, default=100)
 parser.add_argument('--lr', help='learning rate', type=float, default=0.0001)
-parser.add_argument(
-    '--save_every', help='Number of iterations before saying', type=int, default=1000)
+parser.add_argument('--save_every',
+                    help='Number of iterations before saying',
+                    type=int,
+                    default=1000)
 parser.add_argument('--save_dir', help='save dir', type=str, default='save/')
 args = parser.parse_args()
 
 print(args)
 # convert names to numpy array
-names_input, names_output, char, vocab, labels, length = load_data(
+names, name_probs, c_to_n_vocab, n_to_c_vocab, sos_idx, eos_idx = load_data(
     args.prop_file)
-vocab_size = len(char)
-inv_vocab = {v: k for k, v in vocab.items()}
+
+SOS = n_to_c_vocab[sos_idx]
+EOS = n_to_c_vocab[eos_idx]
 
 # make save_dir
 if not os.path.isdir(args.save_dir):
     os.mkdir(args.save_dir)
 
 # divide data into training and test set
-num_train_data = int(len(names_input)*0.75)
-train_names_input = names_input[0:num_train_data]
-test_names_input = names_input[num_train_data:-1]
+num_train_data = int(len(names) * 0.75)
+num_test_data = len(names) - num_train_data
 
-train_names_output = names_output[0:num_train_data]
-test_names_output = names_output[num_train_data:-1]
-
-train_length = length[0:num_train_data]
-test_length = length[num_train_data:-1]
-
-model = VAE(vocab_size,args)
-print('Number of parameters : ', np.sum(
-    [np.prod(v.shape) for v in tf.trainable_variables()]))
+model = VAE(len(c_to_n_vocab), args)
+print('Number of parameters : ',
+      np.sum([np.prod(v.shape) for v in tf.trainable_variables()]))
 
 for epoch in range(args.num_epochs):
     train_loss = []
     test_loss = []
-    for iteration in range(len(train_names_input)//args.batch_size):
-        n = np.random.randint(len(train_names_input), size=args.batch_size)
-        x = np.array([train_names_input[i] for i in n])
-        y = np.array([train_names_output[i] for i in n])
-        l = np.array([train_length[i] for i in n])
+    for iteration in range(num_train_data // args.batch_size):
+        x, y, l = create_batch(names, name_probs, args.batch_size,
+                               c_to_n_vocab, SOS, EOS)
+        x = np.array(x)
+        y = np.array(y)
+        l = np.array(l)
+
         try:
             cost = model.train(x, y, l)
         except Exception as e:
@@ -65,16 +67,17 @@ for epoch in range(args.num_epochs):
 
         train_loss.append(cost)
 
-        if iteration%args.save_every == 0:
-            ckpt_path = args.save_dir+f'/{args.name}.ckpt'
+        if iteration % args.save_every == 0:
+            ckpt_path = args.save_dir + f'/{args.name}.ckpt'
             model.save(ckpt_path, epoch)
             plot_losses(train_loss, filename=f'{args.name}_train.png')
 
-    for iteration in range(len(test_names_input)//args.batch_size):
-        n = np.random.randint(len(test_names_input), size=args.batch_size)
-        x = np.array([test_names_input[i] for i in n])
-        y = np.array([test_names_output[i] for i in n])
-        l = np.array([test_length[i] for i in n])
+    for iteration in range(num_test_data // args.batch_size):
+        x, y, l = create_batch(names, name_probs, args.batch_size,
+                               c_to_n_vocab, SOS, EOS)
+        x = np.array(x)
+        y = np.array(y)
+        l = np.array(l)
 
         try:
             _, cost = model.test(x, y, l)
--- utils.py
+++ utils.py
@@ -7,29 +7,43 @@ import os
 from os import path
 
 
-def load_data(n):
+def load_data(n: str, SOS: str = '[', EOS: str = ']'):
     df = pd.read_csv(n)
     names = df['name'].tolist()
-    seq_length = df['name'].str.len().max()
-
-    SOS = '['
-    EOS = ']'
+    name_probs = df['probs'].tolist()
     chars = string.ascii_letters + SOS + EOS
+    c_to_n_vocab = dict(zip(chars, range(len(chars))))
+    n_to_c_vocab = dict(zip(range(len(chars)), chars))
 
-    vocab = dict(zip(chars, range(len(chars))))
+    sos_idx = c_to_n_vocab[SOS]
+    eos_idx = c_to_n_vocab[EOS]
 
-    length = np.array([len(n)+1 for n in names])
-    names_input = [(SOS+s).ljust(seq_length, EOS) for s in names]
-    names_output = [s.ljust(seq_length, EOS) for s in names]
-    names_input = np.array([np.array(list(map(vocab.get, s)))
-                            for s in names_input])
-    names_output = np.array([np.array(list(map(vocab.get, s)))
-                             for s in names_output])
-    prop = np.array([] for i in range(len(names)))
-    return names_input, names_output, chars, vocab, prop, length
+    return names, name_probs, c_to_n_vocab, n_to_c_vocab, sos_idx, eos_idx
 
 
-def plot_losses(losses, folder: str = "plot", filename: str = "checkpoint.png"):
+def create_batch(all_names: list, probs_list: list, batch_size: int,
+                 vocab: dict, SOS: str, EOS: str):
+    num_names = len(all_names)
+    samples = np.random.choice(num_names,
+                               batch_size,
+                               p=probs_list / np.sum(probs_list))
+    names = [all_names[samples[i]] for i in range(batch_size)]
+
+    seq_length = len(max(names, key=len)) + 1
+
+    # Names length should be length of the name + SOS xor EOS
+    names_length = [len(n) + 1 for n in names]
+    names_input = [(SOS + s).ljust(seq_length, EOS) for s in names]
+    names_input = [list(map(vocab.get, s)) for s in names_input]
+    names_output = [(s).ljust(seq_length, EOS) for s in names]
+    names_output = [list(map(vocab.get, s)) for s in names_output]
+
+    return names_input, names_output, names_length
+
+
+def plot_losses(losses,
+                folder: str = "plot",
+                filename: str = "checkpoint.png"):
     if not path.exists(folder):
         os.mkdir(folder)
 
